1f6f2aca9d1c02519bf27e7b740c313230f216c6
Mahdi <Mahdi@smartvaniya.ir>
2023-06-24 01:15:41 +0330

    Fetching data from database and rendering it

---
+++ app/components/Price.tsx
@@ -0,0 +1,32 @@
+import { PRICE } from '@prisma/client'
+import React from 'react'
+
+export default function Price({ price }: { price: PRICE }) {
+    const renderPrice = () => {
+        if (price === PRICE.CHEAP) {
+            return (
+                <>
+                    <span>$$</span> <span className='text-gray-400'>$$</span>
+                </>
+            )
+        }
+        if (price === PRICE.REGULAR) {
+            return (
+                <>
+                    <span>$$$</span> <span className='text-gray-400'>$</span>
+                </>
+            )
+        }
+        if (price === PRICE.EXPENSIVE) {
+            return (
+                <>
+                    <span>$$$$</span>
+                </>
+            )
+        }
+    }
+
+    return (
+        <p className="mr-3 flex">{renderPrice()}</p>
+    )
+}
